93bb65e78b8125ff2a1f421d138bcb7118579d9c
bvernot <bvernot@gmail.com>
2015-02-14 23:35:08 -0800

    Added create-bed.

--- bin/myBedTools4.py
+++ bin/myBedTools4.py
@@ -25,10 +25,17 @@ def parse_arguments():
 
     subparsers = parser.add_subparsers(dest='func')
 
-    infile_parser = argparse.ArgumentParser(add_help = False)
-    infile_parser.add_argument('-fa', '--fasta-files', type=argparse.FileType('r'), default=None, nargs='+', required=True)
+    infile_seq_parser = argparse.ArgumentParser(add_help = False)
+    infile_seq_parser.add_argument('-fa', '--fasta-files', type=argparse.FileType('r'), default=None, nargs='+', required=True)
+
+    infile_bed_parser = argparse.ArgumentParser(add_help = False)
+    infile_bed_parser.add_argument('-bed', '--bed-files', type=argparse.FileType('r'), default=None, nargs='+', required=True)
+    # infile_bed_parser.add_argument('-fa', '--fasta-files', type=argparse.FileType('r'), default=None, nargs='+', required=False)
     # parser.add_argument('-vcf', '--vcf-file', required=False, type=argparse.FileType('r'), default=None)
     # parser.add_argument('-vcfz', '--gzip-vcf-file', required=False, default=None)
+
+    bed_parser = argparse.ArgumentParser(add_help = False)
+    bed_parser.add_argument('-int-chrs', '--int-chrs', default=False)
     
     compression_parser = argparse.ArgumentParser(add_help = False)
     compression_parser.add_argument('-clev', '--compression-level', type=int, default=4)
@@ -41,12 +48,14 @@ def parse_arguments():
     filetype_parser = argparse.ArgumentParser(add_help = False)
     filetype_parser.add_argument('filetype', choices=['seq', 'bed'])
 
-    create_file_parser = subparsers.add_parser('create', parents = [filetype_parser, infile_parser, compression_parser])
+    create_seq_file_parser = subparsers.add_parser('create-seq', parents = [infile_seq_parser, compression_parser])
+    create_bed_file_parser = subparsers.add_parser('create-bed', parents = [infile_bed_parser, compression_parser, bed_parser])
     cat_file_parser = subparsers.add_parser('cat', parents = [binary_infile_parser])
-    cat_file_parser = subparsers.add_parser('peek', parents = [binary_infile_parser])
+    peek_file_parser = subparsers.add_parser('peek', parents = [binary_infile_parser])
     
     opts = parser.parse_args()
     return opts
+
 
 
 def create_compressed_sequence_file_from_fasta(opts):
@@ -117,20 +126,45 @@ def create_compressed_bed_file_from_bed(opts):
     f.attrs['filetype'] = 'bed'
     f.attrs['ref_version'] = opts.reference_version
 
-    ## this seems like a crappy way to make sure all chromosomes are defined..
-    chrname = 'chr1'
-    num_bases = 249250621
-    cdef = f.create_dataset(chrname, (num_bases,), dtype='b', compression='gzip', compression_opts=opts.compression_level)
-
-    current_progress = 0
-    for x in xrange(0,num_bases,2):
-        cdef[x] = 1
-        if opts.progress > 0 and opts.progress <= x - current_progress:
-            current_progress = x
-            sys.stderr.write("progress.. %s %d\n" % (chrname, x))
-            pass
+    ## make sure all chromosomes are defined..
+    for chrname, num_bases in refDefinitions.chr_lens.items():
+        print "Initializing chromosome %s, with %d bases" % (chrname, num_bases)
+        cdef = f.create_dataset(chrname, (num_bases,), dtype='b', compression='gzip', compression_opts=opts.compression_level, fillvalue=0)
+        # cdef[:] = 0
         pass
 
+    ## find out if the chromosomes are 1 or chr1
+    ## THIS DOESN'T WORK WITH STDIN
+    int_chrs = False
+    test_line = opts.bed_files[0].readline()
+    test_chr = test_line.split()[0]
+    if test_chr not in refDefinitions.chr_lens and ('chr' + test_chr) in refDefinitions.chr_lens:
+        int_chrs = True
+    elif test_chr not in refDefinitions.chr_lens:
+        print "Can't find chromosome in hg19 reference?"
+        print test_chr
+        print test_line
+        sys.exit(-1)
+        pass
+    opts.bed_files[0].seek(0)
+
+    current_progress = 0
+    for bedfile in opts.bed_files:
+        for l in bedfile:
+            l = l.rstrip().split()
+            start = int(l[1])
+            end = int(l[2])
+            chr = 'chr' + l[0] if int_chrs else l[0]
+
+            f[chr][start:end] = 1
+
+            if opts.progress > 0 and opts.progress <= start - current_progress:
+                current_progress = start
+                sys.stderr.write("progress.. %s %d\n" % (chrname, start))
+                pass
+            pass
+        pass
+    
     f.close()
     
     return
@@ -142,19 +176,30 @@ def to_bed(chrom, limit=None):
     pos = 0
     count = 0
 
+    chrname = chrom.name
+
+    if limit != None:
+        chrom = chrom[:10000]
+        pass
+    
     for k,g in groupby(chrom):
         l = sum(1 for x in g)
         
-        if k == 0: 
+        if k == 0:
             pos += sum(1 for x in g)
             continue
         
-        print "%s\t%d\t%d" % (chrom.name, pos, pos+l)
+        print "%s\t%d\t%d" % (chrname, pos, pos+l)
         pos += l
 
         count += 1
         if limit != None and count >= limit: break
         pass
+    
+    if limit != None and count < limit:
+        # print "no elements found within 10000 bases"
+        pass
+
     pass
 
 
@@ -196,6 +241,36 @@ def peek_compressed_file(opts):
     return
 
 
+class refDefinitions:
+
+    chr_lens = {'chr1': 249250621,
+                'chr2': 243199373,
+                'chr3': 198022430,
+                'chr4': 191154276,
+                'chr5': 180915260,
+                'chr6': 171115067,
+                'chr7': 159138663,
+                'chr8': 146364022,
+                'chr9': 141213431,
+                'chr10': 135534747,
+                'chr11': 135006516,
+                'chr12': 133851895,
+                'chr13': 115169878,
+                'chr14': 107349540,
+                'chr15': 102531392,
+                'chr16': 90354753,
+                'chr17': 81195210,
+                'chr18': 78077248,
+                'chr19': 59128983,
+                'chr20': 63025520,
+                'chr21': 48129895,
+                'chr22': 51304566,
+                'chrX' : 155270560,
+                'chrY' : 59373566,
+                'chrM' : 16569}
+
+    pass
+
 
 class myBedTools:
 
@@ -210,9 +285,9 @@ if __name__ == "__main__":
 
     opts = parse_arguments()
 
-    if opts.func == 'create' and opts.filetype == 'seq':
+    if opts.func == 'create-seq':
         create_compressed_sequence_file_from_fasta(opts)
-    elif opts.func == 'create' and opts.filetype == 'bed':
+    elif opts.func == 'create-bed':
         create_compressed_bed_file_from_bed(opts)
     elif opts.func == 'cat':
         cat_compressed_file(opts)
