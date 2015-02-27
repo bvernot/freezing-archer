b6e6508f0ba95ff58d4c3864f88b9c5e20e23d2b
Benjamin Vernot <bvernot@gmail.com>
2015-02-27 13:06:44 -0800

    updated ms_to_vcf, added read_ms, consolidating their methods.

---
+++ bin/ms_to_vcf.py
@@ -0,0 +1,73 @@
+from __future__ import division
+import sys, itertools
+import argparse
+from read_ms import process_ms_block_to_genotypes
+
+
+import time
+start_time = time.time()
+
+import locale
+locale.setlocale(locale.LC_ALL, 'en_US')
+
+
+
+parser = argparse.ArgumentParser(description='Convert an ms file to a VCF file - in a very basic way.')
+parser.add_argument('msfile', type=argparse.FileType('r'), default=sys.stdin)
+parser.add_argument('-o', '--output-file', type=argparse.FileType('w'), default=sys.stdout)
+parser.add_argument('-d', '--debug', action='store_true')
+parser.add_argument('-winlen', '--window-length', type=int, default=50000)
+parser.add_argument('-winstep', '--window-step', type=int, default=20000)
+
+opts = parser.parse_args()
+
+# ms 42 1 -s 10 
+# 44126 40565 42561
+
+# //
+# segsites: 10
+# positions: 0.1717 0.2230 0.2277 0.4523 0.4598 0.5201 0.7094 0.8533 0.9100 0.9894 
+# 0010000001
+# 0110000001
+# 1000000000
+# 1000000000
+# 0010000001
+
+# header
+header = opts.msfile.readline().strip().split()
+
+# parse params from header
+(_, nsam, howmany) = header[:3]
+nsam = int(nsam)
+num_inds = nsam // 2
+#setattr(opts, 'num_inds', int(nsam) // 2)
+
+if nsam != 2 * num_inds:
+    sys.stderr.write('Not an even number of chromosomes - dropping last haplotype.')
+    pass
+
+## print header
+print '\t'.join(['#CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO', 'FORMAT'] + ['i%d' % d for d in xrange(1,num_inds+1)])
+
+# seed params
+seed_params = opts.msfile.readline()
+
+line = opts.msfile.readline()
+while not line.startswith('//'):
+    if opts.debug: print line
+    line = opts.msfile.readline()
+    pass
+
+snps = process_ms_block_to_genotypes(opts.msfile, num_inds, opts.window_length)
+
+if opts.debug: 
+    snps = list( snps )
+    print snps
+    pass
+
+blocknum = 1
+for pos, gts in snps:
+    print '\t'.join(str(s) for s in ['ms%d' % blocknum, pos, '.', 'A', 'G', '.', '.', '.', 'GT'] + ['%d|%d' % (gt[0], gt[1]) for gt in gts])
+    pass
+
+
--- bin/read_ms.py
+++ bin/read_ms.py
@@ -15,8 +15,11 @@ import sys, itertools, operator
 
 
 #@profile
-def process_ms_block_to_genotypes(ms_file , opts):
-
+def process_ms_block_to_genotypes(ms_file , num_inds, window_length):
+    """
+    Process one ms block into positions and genotypes.
+    Returns ((pos, gts), ..); a generator that returns consecutive positions and genotypes.
+    """
 
     ##  assume that the // line has just been consumed
 
@@ -25,7 +28,6 @@ def process_ms_block_to_genotypes(ms_file , opts):
     while not line.startswith('segsites'):
         line = ms_file.readline().strip()
         if line == '':
-            print "EOF"
             return None
         pass
 
@@ -36,15 +38,13 @@ def process_ms_block_to_genotypes(ms_file , opts):
         line = ms_file.readline().strip()
         pass
     
-    positions = list(itertools.imap(float, line.split()[1:]))
-    print positions
-
-
+    # map the space [0,window_length)
+    positions = list(itertools.imap(lambda s : int(float(s) * (window_length-1)), line.split()[1:]))
 
     ## read genotypes, one ind at a time
     in_genotypes = False
-    ind_genotypes = [None] * opts.num_inds
-    for ind_num in range(opts.num_inds):
+    ind_genotypes = [None] * num_inds
+    for ind_num in range(num_inds):
 
         c1_line = ms_file.readline().strip()
         ## make sure we're actually at a genotype line (could be an L or ages line)
@@ -54,8 +54,8 @@ def process_ms_block_to_genotypes(ms_file , opts):
             pass
         in_genotypes = True
         c2_line = ms_file.readline().strip()
-        print c1_line
-        print c2_line
+        # print c1_line
+        # print c2_line
 
         ## check to make sure that we actually have two genotype lines
         if c1_line[0] not in ('0', '1') or c2_line[0] not in ('0', '1'):
@@ -68,19 +68,29 @@ def process_ms_block_to_genotypes(ms_file , opts):
         c1_line = itertools.imap(int, c1_line)
         c2_line = itertools.imap(int, c2_line)
 
-        ind_genotypes[ind_num] = itertools.imap(operator.add, c1_line, c2_line)
+        # ind_genotypes[ind_num] = itertools.imap(operator.add, c1_line, c2_line)
+        ind_genotypes[ind_num] = itertools.izip(c1_line, c2_line)
         pass
+
+    return itertools.izip(positions, itertools.izip(*ind_genotypes))
+
+
+def process_ms_block_to_snp_list(ms_file, opts):
+
+    """
+    Process one ms block into a list of snp structures
+    returns snps.
+    """
     
     snps = ['hey'] * segsites
 
-    for snp_num, genotypes in enumerate(itertools.izip(*ind_genotypes)):
+    for snp_num, (pos, genotypes) in enumerate(process_ms_block_to_genotypes(ms_file, opts.num_inds, opts.window_length)):
+
         snp_d = {}
-        snp_d['pos'] = int(positions[snp_num] * opts.window_length)
+        snp_d['pos'] = pos
 
         t_gt = list(itertools.imap(lambda j : genotypes[j], opts.updated_ind_indexed_to_orig_file_target))
         r_gt = list(itertools.imap(lambda j : genotypes[j], opts.updated_ind_indexed_to_orig_file_reference))
-        print t_gt
-        print r_gt
         genotypes = t_gt + r_gt
 
         snp_d['genotypes'] = genotypes
@@ -89,90 +99,11 @@ def process_ms_block_to_genotypes(ms_file , opts):
 
         snps[snp_num] = snp_d
         
-        print snp_d
-        print opts
         pass
 
 
-
-    print snps
-
     return snps
     
-    # snp_d = {}
-    
-    # split_line = line.strip().split()
-    #     #CHROM  POS     ID      REF     ALT     QUAL    FILTER  INFO    FORMAT  HG00096 HG00097
-    # snp_d['chrom'] = split_line[0]
-    # snp_d['pos'] = int(split_line[1])
-    # snp_d['ref'] = split_line[3]
-    # snp_d['alt'] = split_line[4]
-    # snp_d['qual'] = split_line[5]
-    # snp_d['filt'] = split_line[6]
-    # snp_d['info'] = split_line[7]
-    # format = split_line[8]
-
-    # ## remove non-biallelic sites
-    # if len(snp_d['ref']) != 1 or len(snp_d['alt']) != 1:
-    #     if opts.debug: print "dropping snp because it is not biallelic, or is an indel"
-    #     return None
-
-    # ## get genotypes
-    # genotypes = split_line[9:]
-
-    # ## remove individual genotypes that aren't in the target or reference, and reorder (target first, then ref)
-    # ## (reordered ind ids in read_1kg_ind_pop_file)
-    # r_gt = list(itertools.imap(lambda j : genotypes[j], opts.updated_ind_indexed_to_orig_file_reference))
-    # t_gt = list(itertools.imap(lambda j : genotypes[j], opts.updated_ind_indexed_to_orig_file_target))
-
-
-    # genotypes = t_gt + r_gt
-    # #print t_gt
-    # #print r_gt
-    # #print genotypes
-    # t_gt = set(t_gt)
-    # r_gt = set(r_gt)
-
-    # snp_d['target'] = '0|1' in t_gt or '1|0' in t_gt or '1|1' in t_gt
-    # snp_d['reference'] = not('0|0' in r_gt and len(r_gt) == 1)
-
-    # ## finally, remove variants that aren't in the target or reference
-    # if not snp_d['target'] and not snp_d['reference']:
-    #     if opts.debug: print "dropping snp because it's NOT present in either the TARGET or REFERENCE"
-    #     return None
-
-
-    # ## get the location of genotypes, if necessary
-    # if True or ':' in format:
-    #     gt_loc = format.split(':')
-    #     if 'GT' not in gt_loc:
-    #         print "bad format?  GT not found"
-    #         print format
-    #         print split_line
-    #         sys.exit(-1)
-    #         pass
-    #     gt_loc = gt_loc.index('GT')
-    
-    #     ## strip out just genotypes
-    #     # genotypes[:] = [gt.split(':')[gt_loc] for gt in genotypes]
-    #     genotypes[:] = list(itertools.imap(lambda gt : gt.split(':')[gt_loc], genotypes))
-    #     pass
-    
-    # if opts.debug: print 'reading', snp_d['pos'], genotypes
-
-    # gt_map = {'0|0':0, '1|0':1, '0|1':1, '1|1':2}
-    # gt2 = list(itertools.imap(lambda gt : gt_map[gt], genotypes))
-    # genotypes[:] = list(itertools.imap(lambda gt : gt_map[gt], genotypes))
-    # if opts.debug: print genotypes
-
-
-    # snp_d['genotypes'] = genotypes
-
-
-
-    # if opts.debug: print
-
-    # return snp_d
 
 
 
